60d62ea5a7b375ea7d161e75094c34acfe93dd3a
Chun Liu <chunliu@microsoft.com>
2020-09-13 16:41:22 +0800

    add health probe

---
+++ src/aspnetcore-aad/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore_aad.Controllers
+{
+    [AllowAnonymous]
+    public class HealthController : Controller
+    {
+        // GET: HealthController
+        public ActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
